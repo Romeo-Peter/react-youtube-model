07f56b077106adf732fbd9d01e5627645f35dfc5
Romeo <romeomeshack34@gmail.com>
2019-11-15 21:31:38 +0100

    Controlled input through React 'state'

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
+// Functional component
+
+/*const SearchBar = () => {
     return <input />;
-}
+}*/
 
+// Class base component
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {SearchTerm: 'starting value'};
+    }
+
+    render() {
+
+        return(
+            <div> 
+                <input
+                value = {this.state.SearchTerm} 
+                onChange={(e) => this.setState({ SearchTerm: e.target.value} )} />
+            </div>
+        )
+    }
+}
 export default SearchBar;
